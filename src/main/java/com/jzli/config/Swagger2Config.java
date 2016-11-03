72f2e6a442835eab6c85c629ecf61166ef108c0a
李金钊 <li.jinzhao@jyall.com>
2016-11-03 12:54:43 +0800

    修改配置文件--使用阿里云仓库

--- src/main/java/com/jzli/config/Swagger2Config.java
+++ src/main/java/com/jzli/config/Swagger2Config.java
@@ -1,5 +1,6 @@
 package com.jzli.config;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -7,6 +8,9 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * =======================================================
@@ -23,9 +27,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
     @Bean
     public Docket swaggerApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_12)
                 .apiInfo(apiInfo())
                 .select()
+                .paths(petstorePaths())
                 .build();
     }
 
@@ -35,5 +40,11 @@ public class Swagger2Config {
 //                .description("SpringBoot_Swagger2")
                 .version("1.0")
                 .build();
+    }
+
+    private Predicate<String> petstorePaths() {
+        return or(
+                regex("/user.*")
+        );
     }
 }
